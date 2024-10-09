91a5b85da29177088732b9e87a573cac9f6fcda5
YolanC13 <yolan.chiotti@ynov.com>
2024-10-09 10:36:33 +0200

    Challenge2 terminé

--- main.go
+++ main.go
@@ -21,6 +21,11 @@ type Etudiant struct {
 	Sexe   string
 }
 
+type View struct {
+	ViewCounter int
+	IsOdd       bool
+}
+
 func main() {
 	Page := Page{
 		NomClasse:      "B1 Informatique",
@@ -32,14 +37,30 @@ func main() {
 			{Nom: "Nom", Prenom: "Prénom", Age: 21, Sexe: "F"},
 		},
 	}
+
+	View := View{
+		ViewCounter: 0,
+		IsOdd:       false,
+	}
+
 	temp, errTemp := template.ParseGlob("promo/*.html")
 	if errTemp != nil {
 		fmt.Printf("Error: %v\n", errTemp)
 		return
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
 		temp.ExecuteTemplate(w, "index", Page)
+	})
+
+	http.HandleFunc("/change", func(w http.ResponseWriter, r *http.Request) {
+		View.ViewCounter++
+		if View.ViewCounter%2 == 1 {
+			View.IsOdd = true
+		} else {
+			View.IsOdd = false
+		}
+		temp.ExecuteTemplate(w, "change", View)
 	})
 
 	http.ListenAndServe("localhost:8080", nil)
