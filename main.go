03ba011cabbbc12710d585cfbf49709e1c3b9c8e
YolanC13 <yolan.chiotti@ynov.com>
2024-10-10 10:24:54 +0200

    Challenge 3 terminé

--- main.go
+++ main.go
@@ -26,6 +26,16 @@ type View struct {
 	IsOdd       bool
 }
 
+type Utilisateur struct {
+	Nom           string
+	Prenom        string
+	DateNaissance string
+	Sexe          string
+	Errors        []string
+}
+
+var utilisateurData Utilisateur
+
 func main() {
 	Page := Page{
 		NomClasse:      "B1 Informatique",
@@ -63,5 +73,39 @@ func main() {
 		temp.ExecuteTemplate(w, "change", View)
 	})
 
+	http.HandleFunc("/user/form", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "userForm", Page)
+	})
+
+	http.HandleFunc("/user/treatment", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			utilisateurData.Errors = nil
+			r.ParseForm()
+			utilisateurData.Nom = r.FormValue("nom")
+			utilisateurData.Prenom = r.FormValue("prenom")
+			utilisateurData.DateNaissance = r.FormValue("age")
+			utilisateurData.Sexe = r.FormValue("sexe")
+			if len(utilisateurData.Nom) < 1 || len(utilisateurData.Nom) > 32 {
+				utilisateurData.Errors = append(utilisateurData.Errors, "Le nom doit être compris entre 1 et 32 caractères")
+			}
+			if len(utilisateurData.Prenom) < 1 || len(utilisateurData.Prenom) > 32 {
+				utilisateurData.Errors = append(utilisateurData.Errors, "Le prénom doit être compris entre 1 et 32 caractères")
+			}
+			if utilisateurData.Errors != nil {
+				http.Redirect(w, r, "/user/error", http.StatusSeeOther)
+			}
+			http.Redirect(w, r, "/user/display", http.StatusSeeOther)
+		}
+	})
+
+	http.HandleFunc("/user/display", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "userDisplay", utilisateurData)
+	})
+
+	http.HandleFunc("/user/error", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "userError", utilisateurData)
+	})
+
+	http.Handle("/promo/", http.StripPrefix("/promo/", http.FileServer(http.Dir("./promo"))))
 	http.ListenAndServe("localhost:8080", nil)
 }
